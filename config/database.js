033d7f796ec621265827077a91395508a7469b16
username <youremail@domain.com>
2023-08-02 18:45:32 -0400

    switched psql src

--- config/database.js
+++ config/database.js
@@ -1,13 +1,16 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "chatapp"),//Name of database
-      user: env("DATABASE_USERNAME", "postgres"),//Default username
-      password: env("DATABASE_PASSWORD", "12345678"),//Password to your PostgreSQL database
-      ssl: env.bool("DATABASE_SSL", false),
+      host: env("DATABASE_HOST", config.host),
+      port: env.int("DATABASE_PORT", config.port),
+      database: env("DATABASE_NAME", config.database),
+      user: env("DATABASE_USERNAME", config.user),
+      password: env("DATABASE_PASSWORD", config.password),
+      ssl: { rejectUnauthorized: false },
     },
   },
 });
